1d0d5940ac2637a129c3f9e20759a94e4662155a
Hamidreza <momenih333@gmail.com>
2024-11-08 14:14:41 +0330

    create route and view and controller for categoryVideo which shows all videos based on thier category

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\VideosController;
+use App\Http\Controllers\CategoryVideoController;
 
 Route::get('/', [IndexController::class, 'index'])->name('index');
 
@@ -12,3 +13,5 @@ Route::post('/videos', [VideosController::class, 'store'])->name('videos.store')
 Route::get('videos/{video:slug}', [VideosController::class, 'show'])->name('videos.show');
 Route::get('videos/{video}/edit', [VideosController::class, 'edit'])->name('videos.edit');
 Route::put('videos/{video}', [VideosController::class, 'update'])->name('videos.update');
+
+Route::get('/categories/{category:slug}/videos', [CategoryVideoController::class, 'index'])->name('categories.videos.index');
