f6e638e68453ce7b7a9737496fda5f0f5c38e219
Hamidreza <momenih333@gmail.com>
2024-11-08 14:15:09 +0330

    add category_id to videoFactory

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -24,7 +25,8 @@ class VideoFactory extends Factory
             'thumbnail' => 'https://loremflickr.com/446/240/world?random=' . rand(1, 99),
             'length' => fake()->randomDigit(),
             'slug' => fake()->slug(3),
-            'description' => $persian_faker->realText()
+            'description' => 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته،',
+            'category_id' => Category::all()->random(1)->first()->id
         ];
     }
 }
